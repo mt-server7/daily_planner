499d7eb9145ca0d2227c7faed08561b1ff7cb6e6
Михаил Т <m.t-server7@yandex.ru>
2023-05-31 09:34:28 +0300

    Переработано меню

--- file/class_menu.cpp
+++ file/class_menu.cpp
@@ -1,6 +1,81 @@
 #include <iostream>
+#include "class_data_base.cpp"
 
-class Menu
+class Menu_action
+{
+    public:
+    void menu_action_db()
+{
+    for(;;)
+    {
+        std::cout<<"Введите действие: "<<std::endl;
+        int num;
+        std::cin>>num;
+        switch(num)
+        {
+            case 99:
+                Data_base var1;
+                var1.open_db();
+                Data_base var2;
+                var2.create_db();
+                break;
+            case 1:
+                DB_insert var3;
+                var3.create_event();
+                break;
+            case 2:
+                DB_select var4;
+                var4.select_event();
+                break;
+            case 3:
+                DB_update var5;
+                var5.update_event();
+                break;
+            case 4:
+                DB_delete var6;
+                var6.delete_event();
+                break;
+            case 5:
+                exit(1);
+            default: std::cout<<"Неверное действие!"<<std::endl;
+        }
+    }
+}
+
+    void menu_action_arr()
+    {
+        for(;;)
+        {
+        std::cout<<"Введите действие: "<<std::endl;
+        int num;
+        std::cin>>num;
+        switch(num)
+        {
+            case 1:
+                std::cout<<1;
+                break;
+            case 2:
+                std::cout<<2;
+                break;
+            case 3:
+                std::cout<<3;
+                break;
+            case 4:
+                std::cout<<4;
+                break;
+            case 5:
+                exit(1);
+
+            default: std::cout<<"Неверное действие!"<<std::endl;
+        }
+    }
+    }
+
+};
+
+
+
+class Menu: Menu_action
 {
 public:
     void menu_txt()
@@ -10,24 +85,50 @@ public:
 
             if(num == 2)
             {
+                for(int i = 0; i<=30; i++)
+                {
+                    std::cout<<"*";
+                }
+                std::cout<<"\n";
                 std::cout<<"Вас приветствует ежедневник!"<<std::endl;
                 std::cout<<"1.Создать событие"<<std::endl;
                 std::cout<<"2.Просмотреть события"<<std::endl;
                 std::cout<<"3.Редактировать событие"<<std::endl;
                 std::cout<<"4.Удалить событие"<<std::endl;
                 std::cout<<"5.Выход"<<std::endl;
+                for(int i = 0; i<=30; i++)
+                {
+                    std::cout<<"*";
+                }
+                std::cout<<"\n";
+                Menu_action::menu_action_arr();
+
             }
+
             else
             {
+                for(int i = 0; i<=30; i++)
+                {
+                    std::cout<<"*";
+                }
+                std::cout<<"\n";
                 std::cout<<"Вас приветствует ежедневник!"<<std::endl;
-                std::cout<<"11.Создать событие"<<std::endl;
-                std::cout<<"12.Просмотреть события"<<std::endl;
-                std::cout<<"13.Редактировать событие"<<std::endl;
-                std::cout<<"14.Удалить событие"<<std::endl;
-                std::cout<<"15.Выход"<<std::endl;
+                std::cout<<"1.Создать событие"<<std::endl;
+                std::cout<<"2.Просмотреть события"<<std::endl;
+                std::cout<<"3.Редактировать событие"<<std::endl;
+                std::cout<<"4.Удалить событие"<<std::endl;
+                std::cout<<"5.Выход"<<std::endl;
               //  std::cout<<"99.Создать базу программы (обязательно и единократно при первом запуске!)"<<std::endl;
+
+                for(int i = 0; i<=30; i++)
+                {
+                    std::cout<<"*";
+                }
+                std::cout<<"\n";
+                Menu_action::menu_action_db();
             }
 
     }
 
 };
+
